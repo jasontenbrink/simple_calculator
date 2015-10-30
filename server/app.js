74e1fab753974cc33a2690df7ddc1e6007345215
jasontenbrink <jason.tenbrink@gmail.com>
2015-10-30 13:55:22 -0500

    initial commit

---
+++ server/app.js
@@ -0,0 +1,13 @@
+var express = require ('express');
+var app = express();
+var path = require('path');
+app.set("port", process.env.PORT || 3000);
+
+app.get('/*', function(request, response){
+  var file = request.params[0] || 'index.html';
+  response.sendFile(path.join(__dirname,file));
+});
+
+app.listen(app.get('port'), function(){
+  console.log('listening on port: ' + app.get('port'));
+});
